fae5d135c9c9295054f72ab9ec98c87268b593d5
dhopz <david_houpapa@hotmail.com>
2022-06-27 11:13:04 +0100

    java first go

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -16,5 +17,49 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void add_TwoPlusTwo_ReturnsFour() {
+      // Arrange
+      final int expected = 4;
+  
+      // Act
+      final int actual = App.add(2, 2);
+  
+      // Assert
+      Assert.assertEquals(expected, actual);
+    }    
+    @Test
+    public void multiply_FourTimesTwo_ReturnsEight() {
+      // Arrange
+      final int expected = 8;
+  
+      // Act
+      final int actual = App.multiply(4, 2);
+  
+      // Assert
+      Assert.assertEquals(expected, actual);
+    }        
+
+    @Test
+    public void divide_TenDividedTwo_ReturnsFive() {
+      // Arrange
+      final int expected = 5;
+  
+      // Act
+      final int actual = App.divide(10, 2);
+  
+      // Assert
+      Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multiply_FiftyTimesTwo_ReturnsOneHundred() {
+        //Arrange
+        final int expected = 100;
+        //Act
+        final int actual = App.multiply(50,2);
+        //Assert
+        Assert.assertEquals(expected, actual);
     }
 }
